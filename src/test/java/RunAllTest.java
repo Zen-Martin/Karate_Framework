dc096dc02fffa7f4a6e03f0550a49aea41aa3a74
Martin <martin.kuatepk67@gmail.com>
2022-03-21 11:48:44 +0100

    scripts updates

--- src/test/java/RunAllTest.java
+++ src/test/java/RunAllTest.java
@@ -1,12 +1,10 @@
 import com.intuit.karate.Results;
 import com.intuit.karate.Runner;
-import com.intuit.karate.junit5.Karate;
 import net.masterthought.cucumber.Configuration;
 import net.masterthought.cucumber.ReportBuilder;
 import net.masterthought.cucumber.Reportable;
 import net.masterthought.cucumber.json.support.Status;
 import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -20,7 +18,7 @@ class RunAllTest {
     @Test
     public void testAll() {
         System.setProperty("karate.env", "test");
-        Results results = Runner.parallel(getClass(), 5, "target/surefire-reports");
+        Results results = Runner.parallel(getClass(), 2, "target/surefire-reports");
         Assertions.assertTrue(results.getFailCount() == 0, results.getErrorMessages());
     }
 
@@ -28,10 +26,10 @@ class RunAllTest {
     public static void makeCucumberReport() {
         File reportOutputDirectory = new File("target");
         List<String> jsonFiles = new ArrayList<>();
-        jsonFiles.add("C:\\Karate\\Projet\\karatedemo\\target\\surefire-reports\\ui.web.zenityIntranet.json");
-        jsonFiles.add("C:\\Karate\\Projet\\karatedemo\\target\\surefire-reports\\ui.mobile.android.zenityIntranetAndroid.json");
-        jsonFiles.add("C:\\Karate\\Projet\\karatedemo\\target\\surefire-reports\\api.users.json");
-        jsonFiles.add("C:\\Karate\\Projet\\karatedemo\\target\\surefire-reports\\api.usersFail.json");
+        jsonFiles.add(System.getProperty("user.dir")+"\\target\\surefire-reports\\ui.web.zenityIntranet.json");
+        jsonFiles.add(System.getProperty("user.dir")+"\\target\\surefire-reports\\ui.mobile.android.zenityIntranetAndroid.json");
+        jsonFiles.add(System.getProperty("user.dir")+"\\target\\surefire-reports\\api.users.json");
+        jsonFiles.add(System.getProperty("user.dir")+"\\target\\surefire-reports\\api.usersFail.json");
 
         String projectName = "Karate demo";
         Configuration configuration = new Configuration(reportOutputDirectory, projectName);
